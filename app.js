52475aa2ae511854d831c5a8fec87bdc41852853
devluanpereira <luan23107@gmail.com>
2024-08-31 09:12:21 -0300

    Post function added

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const sqlite3 = require('sqlite3').verbose();
 const bodyParser = require('body-parser');
 const multer = require('multer');
 const path = require('path');
+const { error } = require('console');
 
 const app = express();
 app.use(bodyParser.urlencoded({ extended:true }));
@@ -39,6 +40,50 @@ db.serialize(() => {
 
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, "views", "index.html"));
+});
+
+app.get('/posts', (req, res) => {
+    db.all('SELECT * FROM posts ORDER BY created_at DESC', [], (err, rows) => {
+        if (err){
+            throw err;
+        }
+        res.json(rows)
+    })
+})
+
+app.get('/post/:title', (req, res) => {
+    const postTitle = req.params.title.replace(/-/g, '').trim();
+    db.get(
+        'SELECT * FROM posts WHERE TRIM(LOWER(title)) =?',
+        [postTitle.toLowerCase()],
+        (err,row) => {
+            if (err) {
+                res.status(500).json({ error: 'Database error'});
+                return;
+            }
+            if (!row) {
+                res.status(404).json({ error: 'Post not found'});
+                return;
+            }
+            res.json(row);
+        }
+    );
+});
+
+app.post('/add', upload.single('image'), (req, res) => {
+    const {title, category, content} = req.body;
+    const image = req.file ? `/uploads/${req.file.filename}` : null;
+
+    db.run(
+        "INSERT INTO posts (title, category, content, image, created_at) VALUES (?,?,?,?, CURRENT_TIMESTAMP",
+        [title, category, content, image],
+        (err) => {
+            if (err) {
+                return console.log(err.message);
+            }
+            res.redirect('/');
+        }
+    );
 });
 
 app.get('/add', (req, res) => {
