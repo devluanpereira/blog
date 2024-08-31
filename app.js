0fb285345274c71d637e61a0ccd518b7510cea46
devluanpereira <luan23107@gmail.com>
2024-08-31 00:05:54 -0300

    🚀 Bank added

--- app.js
+++ app.js
@@ -8,11 +8,43 @@ const app = express();
 app.use(bodyParser.urlencoded({ extended:true }));
 app.use(express.static('public'))
 
+const db = new sqlite3.Database('./database/datbase.db')
+
+//configurar multer para uploads de arquivos de imagem
+
+const storage = multer.diskStorage({
+    destination: './public/uploads/',
+    filename: (req, file, cb) => {
+        cb(null, Date.now() + path.extname(file.originalname))
+    },
+})
+
+const upload = multer({ storage });
+
+//inicializar banco
+
+db.serialize(() => {
+    db.run(`CREATE TABLE IF NOT EXISTS posts (
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        title TEXT NOT NULL,
+        category TEXT,
+        content TEXT NOT NULL,
+        image TEXT,
+        created_at DATETIME DEFAULT CURRENT_TIMESTAMP
+
+    )`);
+});
+
 // Routes
 
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, "views", "index.html"));
 });
+
+app.get('/add', (req, res) => {
+    res.sendFile(path.join(__dirname, "views", "add.html"));
+});
+
 
 const PORT = process.env.PORT || 3000;
 
